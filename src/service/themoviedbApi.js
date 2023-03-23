7d84c98c0a2fdf5cfdd688b83cbfc2ea2c89f8db
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-24 01:05:22 +0200

    write apiService class with url variables and getting trending today movies

--- src/service/themoviedbApi.js
+++ src/service/themoviedbApi.js
@@ -6,3 +6,22 @@ const byKeyWordUrl = "/search/search-movies";
 const wholeMovieInformationUrl = "/movies/get-movie-details";
 const actorsUrl = "/movies/get-movie-credits";
 const viewsUrl = "/movies/get-movie-reviews"; */
+
+class ApiService {
+    apiKey = "d31c9faeabd85b83726848cf0b50c5a1";
+    baseUrl = "https://api.themoviedb.org/3/movie/550?api_key=";
+    mostPoularUrl = "/trending/get-trending";
+    byKeyWordUrl = "/search/search-movies";
+    wholeMovieInformationUrl = "/movies/get-movie-details";
+    actorsUrl = "/movies/get-movie-credits";
+    viewsUrl = "/movies/get-movie-reviews";
+ 
+
+    async getMostPopularMovies() {
+        const response = await fetch(`${this.baseUrl}${this.apiKey}${this.mostPoularUrl}`);
+        console.log(response);
+    };
+
+};
+
+export const apiService = new ApiService();
