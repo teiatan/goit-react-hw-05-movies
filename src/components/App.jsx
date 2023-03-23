cb38c0f21302422fd3e04355988b203d35a8a3d6
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-24 00:29:46 +0200

    add changing current page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { Header } from "./header/Header";
 import { Main } from "./main/Main";
 
 export const App = () => {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const changeCurrentPage = (pageName) => {
+    setCurrentPage(pageName);
+  };
+  
   return (
     <>
-      <Header />
+      <Header changeCurrentPage={changeCurrentPage}/>
       <Main />
     </>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,22 @@
 import { Ul, HeaderTag, NavTag } from "./Header.styled";
 import { PageContainer } from "components/pageContainer/pageContainer";
 
-export function Header({onClick}) {
+export function Header({changeCurrentPage}) {
+
+    const onChangeCurrentPage = e => {
+        if(e.target.nodeName === 'LI') {
+            changeCurrentPage(e.target.id);
+        };
+    };
+
     return (
         <HeaderTag>
             <PageContainer>
                 <NavTag>
                     <a href="public/index.html">🎬</a>
-                    <Ul>
-                        <li id="home" onClick={onClick}>Home</li>
-                        <li id="movies" onClick={onClick}>Movies</li>
+                    <Ul onClick={onChangeCurrentPage}>
+                        <li id="home">Home</li>
+                        <li id="movies">Movies</li>
                     </Ul>
                 </NavTag>
             </PageContainer>
