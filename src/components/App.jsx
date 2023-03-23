bf3eb35306f9b71d87b5923d6e3da47db3c6533c
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-24 00:19:34 +0200

    add main component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./header/Header";
+import { Main } from "./main/Main";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 };
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,13 @@
+import { PageContainer } from "components/pageContainer/pageContainer";
+
+export function Main() {
+    return (
+        <main>
+            <PageContainer>
+                <div>
+                    
+                </div>
+            </PageContainer>
+        </main>
+    );
+};
