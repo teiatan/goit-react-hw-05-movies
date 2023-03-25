30bb4578156a7a75b4d249747557df45ce8a79ed
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-25 02:20:41 +0200

    get movies by key word to movies page component

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from "react";
 //import { MoviesList } from "components/moviesList/MoviesList";
-//import { apiService } from "service/themoviedbApi";
+import { apiService } from "service/themoviedbApi";
 import { SearchBar } from "components/searchBar/SearchBar";
 
 export function Movies() {
     const [searchInput, setSearchInput] = useState(null);
+    const [movies, setMovies] = useState([]);
 
-
-
+    const searchMovieByKeyWord = (inputValue) => {
+        apiService.getMoviesByKeyWord(inputValue).then(
+            response => setMovies(response)
+        );  
+    };
 
     return (
         <>
-        {!searchInput && <SearchBar />}
+        {!searchInput && <SearchBar handleSearchSubmit={searchMovieByKeyWord}/>}
         
         </>
     );
